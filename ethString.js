b9f697ae7700687b47f396dc3be819603a5566e2
KKudryavtsev <konstantin.k.ed@gmail.com>
2014-05-21 01:50:57 +0100

    more functions covered by tests

--- ethString.js
+++ ethString.js
@@ -1,64 +1,60 @@
-require("./BigInteger");
+var bigInt = require("./BigInteger");
 
-//ARE YOU SURE?
-
-//extending native objects is rarely a good idea; clients of the lib won't expect this.
-//can we change these to be normal functions/objects of some sort?
-
-String.prototype.pad = function(l, r) {
-	if (r === null) {
-		r = l
-		if (!(this.substr(0, 2) == "0x" || /^\d+$/.test(this)))
-			l = 0
-	}
+module.exports = {
+  pad: function(l, r) {
+    if (r === null) {
+      r = l;
+      if (!(this.substr(0, 2) == "0x" || /^\d+$/.test(this)))
+        l = 0;
+    }
     var ret = this.bin();
-	while (ret.length < l)
-		ret = "\0" + ret
-	while (ret.length < r)
-		ret = ret + "\0"
+    while (ret.length < l)
+      ret = "\0" + ret;
+    while (ret.length < r)
+      ret = ret + "\0";
     return ret;
-}
+  },
 
-String.prototype.unpad = function() {
-	var i = this.length;
-	while (i && this[i - 1] == "\0")
-		--i
-	return this.substr(0, i)
-}
+  unpad: function() {
+    var i = this.length;
+    while (i && this[i - 1] == "\0")
+      --i
+    return this.substr(0, i);
+  },
 
-String.prototype.bin = function() {
+  bin: function() {
     if (this.substr(0, 2) == "0x") {
-		bytes = []
-		var i = 2;
-		// Check if it's odd - pad with a zero if so.
-		if (this.length % 2)
-			bytes.push(parseInt(this.substr(i++, 1), 16))
-		for (; i < this.length - 1; i += 2)
-		    bytes.push(parseInt(this.substr(i, 2), 16));
-		return String.fromCharCode.apply(String, bytes);
+      bytes = [];
+      var i = 2;
+      // Check if it's odd - pad with a zero if so.
+      if (this.length % 2)
+        bytes.push(parseInt(this.substr(i++, 1), 16));
+      for (; i < this.length - 1; i += 2)
+        bytes.push(parseInt(this.substr(i, 2), 16));
+      return String.fromCharCode.apply(String, bytes);
     } else if (/^\d+$/.test(this))
-		return bigInt(this).toHex().bin()
+      return bigInt(this).toHex().bin();
 
     // Otherwise we'll return the "String" object instead of an actual string
-    return this.substr(0, this.length)
-}
+    return this.substr(0, this.length);
+  },
 
-String.prototype.unbin = function() {
+  unbin: function() {
     var i, l, o = '';
     for(i = 0, l = this.length; i < l; i++) {
-        var n = this.charCodeAt(i).toString(16);
-        o += n.length < 2 ? '0' + n : n;
+      var n = this.charCodeAt(i).toString(16);
+      o += n.length < 2 ? '0' + n : n;
     }
 
     return "0x" + o;
-}
+  },
 
-String.prototype.dec = function() {
-    return bigInt(this.substr(0)).toString()
-}
+  dec: function() {
+    return bigInt(this.substr(0)).toString();
+  },
 
-String.prototype.hex = function() {
-    return bigInt(this).toHex()
-}
-
+  hex: function() {
+    return bigInt(this).toHex();
+  }
+};
 
--- test/all.js
+++ test/all.js
@@ -1,37 +1,101 @@
 var chai = require("chai"),
     sinon = require("sinon"),
     sinonChai = require("sinon-chai"),
-    eth = require("../eth")();
+    eth = require("../")(),
+    should = require("should");
 
-chai.should();
+chai.use(should);
 chai.use(sinonChai);
 
 describe("Eth", function(){
-  describe("getCoinbase", function(){
+  beforeEach(function(done){
+    eth.ready.then(function(){done();});
+  });
+  
+  describe("coinbase", function(){
     it("should return current coinbase", function(done){
-      eth.getCoinbase(function(coinbase){
-        coinbase.length.should.be.equal(42);
+      var cb;
+      eth.coinbase(function(coinbase){
+        cb = coinbase;
+        coinbase.length.should.be.equal(40);
+      }).then(function(coinbase){ //just checking that promises work too
+        coinbase.should.be.equal(cb);
         done();
       });
     });
   });
 
-  describe("getIsListening", function(){
+  describe("isListening", function(){
     it("should report if the client is mining", function(done){
-      eth.getIsListening(function(isListening){
-        isListening.should.be.equal(true);
+      eth.isListening(function(isListening){
+        isListening.should.be.type("boolean");
         done();
       });
     });
   });
   
-  describe("getIsMining", function(){
+  describe("isMining", function(){
     it("should report if the client is mining", function(done){
-      eth.getIsMining(function(isMining){
+      eth.isMining(function(isMining){
         isMining.should.be.equal(true);
         done();
       });
     });
   });
+
+  describe("balanceAt", function(){
+    var coinbase;
+    beforeEach(function(done){
+      eth.coinbase(function(cb){
+        coinbase = cb;
+        done();
+      });
+    });
+    it("returns balance at an address", function(done){
+      eth.balanceAt({a: coinbase }).then(function(balance){
+        balance.toNumber().should.be.a.Number;
+        done();
+      });
+    });
+  });
+
+  describe("block", function(){
+    var lastBlock;
+    
+    beforeEach(function(done){
+      eth.lastBlock(function(block){
+        lastBlock = block;
+        done();
+      });
+    });
+
+    it("gets a block by hash", function(done){
+      eth.block({ a: lastBlock.hash }).then(function(block){
+        block.hash.should.be.ok;
+        done();
+      });
+    });
+  });
+
+
+  describe("key", function(){
+    it("returns a key", function(done){
+      eth.key(function(key){
+        key.should.be.ok;
+        done();
+      });
+    });
+  });
+
+  describe("keys", function(){
+    it("returns keys", function(done){
+      eth.keys(function(keys){
+        keys.length.should.be.above(0);
+        done();
+      });
+    });
+  });
+
+  
 });
 
